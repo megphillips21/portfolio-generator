41d12a59e81c8dcc8c5bc8f512d912045fc9276d
megphillips21 <megphillips21@gmail.com>
2021-12-01 20:27:56 -0800

    generates html file with cmd line arguments

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const profileDataArgs = process.argv.slice(2);
+// node module to use file system or (fs)
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// bottom of file
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+module.exports = generatePage;
